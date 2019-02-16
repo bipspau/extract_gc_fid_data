8ef14ebf185e4758b29e9910dea1fff6ffb5efca
BipanaPau <bipspau@gmail.com>
2019-02-15 22:50:25 -0500

    add README.md

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
+
 # extract_GC_FID_data
 
 This python script can be used to extract compound(s) of interest from the GC-FID data file
+Note: if you use this script in your project, please cite this as 
+
+Paudel Timilsena B, Seild-Adams I, Tumlinson JH . 2019. Herbivore-specific plant volatiles prime neighboring plants for non-specific defense responses. Paper submitted at New Phytologist.
 
 
 ### how to copy data file from GC-FID connected computer?
 
 1. Copy all the data files in simple text format (not in word document).
-2. Make sure your datafile name has replication, treatment name and time/date of VOC collection and each information separated by underscore (eg. 1_ctrl_04hr)
+2. This script extract information about the treatment, replication and time/date from the file name. Therefore make sure your file name contains these information separated by underscore (eg. 1_ctrl_04hr) 
 
 ### How to run this script in your computer? 
 1. clone or download python script and save it in your working directory
@@ -19,10 +23,12 @@ If you are using windows :
 	go to run and type cmd and hit enter
 	
 3. set your working directory (make sure your python script is saved in your working directory)
+
 Mac:
 	cd Desktop/my_project
 	
 3. run python script 
+
 Python extract_compounds_of_interest.py output_file_name.xls 
 
 Then you will get a excel sheet that contains only the compounds of interest from all the data files. 	
--- extract_compounds_of_interest.py
+++ extract_compounds_of_interest.py
@@ -1,16 +1,11 @@
-# This script was used to extract compounds of interest from GC-FID.
+# This script was used to extract compounds of interest from GC-FID data files.
+# for detail instruction on using this script, recent updates and citation open the following link
+# https://github.com/BipanaPau/extract_GC_FID_data.git 
 
-###### how to copy data file from GC-FID connected computer? ######
-# Copy all the data files in simple text format (not in word document).
-# make sure your datafile name has replication, treatment name and time/date of VOC collection 
-# and separate each word in the file name by underscore (eg. 1_ctrl_04hr)
-
-#### How to run this script in your computer? ####
-MAc - #application > utility > terminal
-Window - 
 
 #!/usr/bin/env python
 import os, sys, re
+
 
 fileout = sys.argv[1]
 file_o = open(fileout, "w")
@@ -24,9 +19,9 @@ col7 = "Quantity"
 
 file_o.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %(col1, col2, col3, col4, col5,col6,col7))
 
-# give path of the datafile
+# navigate your file/folder
 
-os.chdir("20170821_voc_absorption_and_reemission")
+os.chdir("/Users/Desktop/my_project/my_data")
 
 x = 0;	
 for cur_file in os.listdir("."):
@@ -53,10 +48,13 @@ for cur_file in os.listdir("."):
                 #print retn
                 #print area
     
-  # to extract compound of interest from the data files
+  ### to extract compound of interest from the data files ###
+  
+  #Retention time for the same compounds could vary among the treatments.
+  #therefore we give a range of retention time for every compounds
     
-                if (retn >= 4.10 and retn <= 4.17):
-                    dict["Propyl aldoxime, 2 methyl-,syn-"] = str(retn) + "\t"  + str(area)
+                if (retn >= 4.10 and retn <= 4.17):  #replace these numbers by retention time of the compound
+                    dict["Propyl aldoxime, 2 methyl-,syn-"] = str(retn) + "\t"  + str(area) #write the compound name the quotation mark             
                 elif (retn >= 4.23 and retn <= 4.29):
                     dict["Propyl aldoxime, 2 methyl-,anti-"] = str(retn) + "\t"  + str(area)
                 elif (retn >= 5.07 and retn <= 5.13):
@@ -96,7 +94,7 @@ for cur_file in os.listdir("."):
                 elif (retn >= 21.70 and retn <= 21.76):
                     dict["Indole"] = str(retn) + "\t"  + str(area)
                 elif (retn >= 22.35 and retn <= 22.41):
-                    dict["Nonyl_acetate"] = str(retn) + "\t"  + str(area)
+                    dict["Nonyl_acetate"] = str(retn) + "\t"  + str(area) # this is our internal standard
                     nony = float(area)
                 elif (retn >= 22.78 and retn <= 22.83):
                     dict["Z3 Hexenyl tilgate"] = str(retn) + "\t"  + str(area)
@@ -129,6 +127,7 @@ for cur_file in os.listdir("."):
 
 # calculate amount of compounds by comparing peak area of compound of interest with the peak area of internal standard.
 # we used 120ul of elution buffer which contains 480ng of internal standard. 
+# Change that number depending on the concentration of internal standard in total volume of elution buffer
 
             quantity = float(area_new * 480 / nony)
             print ("%s = %s, %s = %s, %s = %f, %s = %f, %s = %f" 
